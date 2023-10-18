e27f0517a17e747e58d326b7806282960052bc30
Fidan Mammadova <100687487+FidanMemmedova@users.noreply.github.com>
2023-10-18 13:48:46 +0400

    Add files via upload

---
+++ SwitchCase/assets/js/main.js
@@ -0,0 +1,19 @@
+let diplom = 2;
+switch (diplom) {
+  case 0:
+    diplom = "Adi Diplom";
+    console.log(diplom);
+    break;
+  case 1:
+    diplom = "Honor Diplom";
+    console.log(diplom);
+    break;
+  case 2:
+    diplom = "High Honor Diplom";
+    console.log(diplom);
+    break;
+
+  default:
+    diplom= "No Diplom"
+    break;
+}
